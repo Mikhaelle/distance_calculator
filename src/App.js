9ac584db5d86ef970909ea62f0b5f8a9e0847594
Mikhaelle <mikhabueno@outlook.com>
2023-05-29 11:23:37 -0300

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-import {Router} from "./Router";
-
-export default function App() {
-  return (
-      <RouterProvider router={Router} />
-  );
-}
